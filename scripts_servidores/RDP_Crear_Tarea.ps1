56e65d02fdf4b143c9fd3bec07a01d9ffea3984a
Diego Zacur <diego.zacur@dvsoft.com.ar>
2023-05-13 02:58:56 -0300

    Usuario

--- scripts_servidores/RDP_Crear_Tarea.ps1
+++ scripts_servidores/RDP_Crear_Tarea.ps1
@@ -11,10 +11,11 @@ If (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 }
 
 $credential = Get-Credential
-Write-Host Usuario $credential.UserName
-Write-Host Clave $credential.Password
+$Usuario = Read-Host "Usuario:"
+$Clave = Read-Host "Clave:"
+
 
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Dejavu75/WebScripts/main/scripts_servidores/RDPTimeBomb.ps1" -OutFile "C:\windows\RDPTimeBomb.ps1" -UseBasicParsing
 $Action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-File C:\windows\RDPTimeBomb.ps1'
 $Trigger =  New-ScheduledTaskTrigger -Weekly  -WeeksInterval 4 -At 2:45am -DaysOfWeek Sunday
-Register-ScheduledTask -Action $Action -Trigger $Trigger -TaskName "Solinges\RDP Timebomb" -Description "Tarea programada generada por la consola." -User $credential.UserName -Password $credential.Password
+Register-ScheduledTask -Action $Action -Trigger $Trigger -TaskName "Solinges\RDP Timebomb" -Description "Tarea programada generada por la consola." -User $Usuario -Password $Clave
