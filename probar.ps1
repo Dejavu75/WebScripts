98372102225344f7d4a70b984cff6af39226c123
Dejavu75 <diego.zacur@dvsoft.com.ar>
2023-05-13 00:58:56 -0300

    Create probar.ps1

---
+++ probar.ps1
@@ -0,0 +1,6 @@
+write-host -back Black -fore Black .
+write-host -back Black -fore Green Script terminado... 
+write-host -back Black -fore Black .
+write-host -back Black -fore Green Presione una tecla para cerrar el PowerShell 
+$Key = [Console]::ReadKey($True)
+exit
